e3936d6634eb3352538c9e76ef5da5e46cd8e448
soselab2020 <shangpinma@gmail.com>
2024-06-10 16:07:39 +0800

    feat: first version

---
+++ FirstClass4Team.java
@@ -0,0 +1,8 @@
+public class FirstClass4Team {
+    // main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("這是用來練習Jenkins的程式!");
+        System.out.println("\"學員：\"大骨翔平\"");
+    } // end method main
+
+} // end class FirstClass
