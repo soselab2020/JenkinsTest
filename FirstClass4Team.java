015835fe4a9ec41cf91516ee47fed0f653e74e14
soselab2020 <shangpinma@gmail.com>
2024-06-10 16:20:16 +0800

    fix: fix the string

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,7 +2,7 @@ public class FirstClass4Team {
     // main method begins execution of Java application
     public static void main(String[] args) {
         System.out.println("這是用來練習Jenkins的程式!");
-        System.out.println("\"學員：\"大骨翔平\"");
+        System.out.println("學員：\"大骨翔平\"");
     } // end method main
 
 } // end class FirstClass
