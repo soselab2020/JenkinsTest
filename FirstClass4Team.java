47f98f219dfc7acc4eff12dca8c3dfd79a00ea26
soselab2020 <shangpinma@gmail.com>
2024-06-10 16:34:36 +0800

    fix: modify string

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,7 +2,7 @@ public class FirstClass4Team {
     // main method begins execution of Java application
     public static void main(String[] args) {
         System.out.println("Jenkins Test!");
-        System.out.println("I am Ohtani!");
+        System.out.println("I am Ohtani (大谷翔平)!");
     } // end method main
 
 } // end class FirstClass
