de715f56928aa131eadad46792f2772a73574b74
soselab2020 <shangpinma@gmail.com>
2024-06-10 16:28:38 +0800

    fix: change encoding

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -1,4 +1,4 @@
-﻿public class FirstClass4Team {
+public class FirstClass4Team {
     // main method begins execution of Java application
     public static void main(String[] args) {
         System.out.println("Jenkins Test!");
